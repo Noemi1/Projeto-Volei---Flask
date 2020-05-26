20e59fb383dfc4f5ceb621900360309b11f25c4f
Otacilio <otaciliogsantos@gmail.com>
2020-05-26 00:37:54 -0300

    CRUD Time

--- controller.py
+++ controller.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 from app import app
-from dao import listar_partidas, listar_membros, listar_times, ver_resultado, obter_time
+from dao import listar_partidas, listar_membros, listar_times, ver_resultado, obter_time, novo_time, remover_time, nova_partida, remover_partida, update_time
+
 
 @app.route('/')
 def index():
@@ -14,33 +15,149 @@ def index():
 
 @app.route('/partidas') # Nao consigo colocar o /partidas/<time_id>
 def partidas():
-    time_id = request.args.get("time_id")
+    time_id = request.args.get('time_id')
     partidas = listar_partidas(time_id)
-    casa = obter_time(time_id)
-    return render_template('partidas.html',
+    return render_template(
+        'partidas.html',
         title="Partidas",
         partidas=partidas,
-        casa=casa
-        
     )
+
+@app.route('/partidas/remover/<partida_id>/')
+def delete_partida(partida_id):
+    remover_partida(partida_id)
+    return redirect('/')
+
+@app.route('/addpartidas/', methods=['GET', 'POST'])
+def addpartidas():
+    time_casa = request.args.get("time_id")
+    casa = obter_time(time_casa)
+    visita = listar_times()
+
+    if request.method == 'POST':
+        form = request.form
+
+        nomeCasa = form.get('nomeCasa')
+        pontosCasa = form.get('pontosCasa')
+        nomeVisitante = form.get('nomeVisitante')
+        pontosVisitantes = form.get('pontosVisitantes')
+        data = form.get('data')
+        localpartida = form.get('localpartida')
+        duracao = form.get('duracao')
+        setstotal = form.get('setstotal')
+        setsvencidos = form.get('setsvencidos')
+        setsperdidos = form.get('setsperdidos')
+        arbitro = form.get('arbitro')
+        fiscal = form.get('fiscal')
+        vencedor = form.get('vencedor')
+
+        nova_partida(
+            nomeCasa,
+            pontosCasa,
+            nomeVisitante,
+            pontosVisitantes,
+            data,
+            localpartida,
+            duracao,
+            setstotal,
+            setsvencidos,
+            setsperdidos,
+            arbitro,
+            fiscal,
+            vencedor
+        )
+        return redirect('/')
+
+    return render_template(
+        'parts/add-partidas.html',
+        casa=casa,
+        visita=visita
+    )
+
 
 @app.route('/entrar/')
 def entrar():
-    return render_template('entrar.html',
+    return render_template(
+        'entrar.html',
         title="Entrar"
     )
 
-@app.route('/addTime/')
-def addTime():
-    return render_template('parts/add-time.html')
 
-@app.route('/editTime/')
-def editTime():
-    return render_template('parts/add-time.html')
+@app.route('/addTime/', methods=['GET', 'POST'])
+def addTime():
+
+    if request.method == 'POST':
+        form = request.form
+
+        Nome = form.get('Nome')
+        Sigla = form.get('Sigla')
+        Localidade = form.get('Localidade')
+        Pontos = form.get('Pontos')
+        Jogos = form.get('Jogos')
+        Vitorias = form.get('Vitorias')
+        Derrotas = form.get('Derrotas')
+
+        novo_time(
+            Nome,
+            Sigla,
+            Localidade,
+            Pontos,
+            Jogos,
+            Vitorias,
+            Derrotas
+        )
+        return redirect('/')
+
+    return render_template(
+        'parts/add-time.html',
+        curso={}
+    )
+
+
+@app.route('/time/remover/<time_id>/')
+def delete_time(time_id):
+    remover_time(time_id)
+    return redirect('/')
+
+
+@app.route('/time/alterar/', methods=['GET', 'POST'])
+def alterar_time():
+    time_id = request.args.get("time_id")
+    time = obter_time(time_id)
+
+    if request.method == 'POST':
+        form = request.form
+
+        Nome = form.get('Nome')
+        Sigla = form.get('Sigla')
+        Localidade = form.get('Localidade')
+        Pontos = form.get('Pontos')
+        Jogos = form.get('Jogos')
+        Vitorias = form.get('Vitorias')
+        Derrotas = form.get('Derrotas')
+
+        update_time(
+            Nome,
+            Sigla,
+            Localidade,
+            Pontos,
+            Jogos,
+            Vitorias,
+            Derrotas,
+            time_id
+        )
+        return redirect('/')
+
+    return render_template(
+        'parts/update_time.html',
+        time=time
+    )
+
 
 @app.route('/addMembro/')
 def addMembro():
     return render_template('parts/add-membro.html')
+
 
 @app.route('/detalhes/') # Nao consigo colocar o /detalhes/<id>
 def detalhes():
--- dao.py
+++ dao.py
@@ -1,25 +1,12 @@
 from flask import g
-from db import query_db
+from db import query_db, execute_db
 
-
-def listar_times():
-    return g.query_db(
-        'SELECT * FROM Equipes ORDER BY Pontos DESC'
-    )
-
-
-def obter_time(time_id):
-    return g.query_db(
-        'SELECT * FROM Equipes WHERE Id = ?',
-        [time_id],
-        one=True
-    )
 
 
 def listar_membros(time_id):
     return g.query_db(
         'SELECT * FROM Membros WHERE Time_Id = ?',
-        [time_id],
+        (time_id)
     )
 
 
@@ -28,10 +15,12 @@ def listar_partidas(time_id):
         '''
             SELECT *
               FROM Partidas
-               INNER JOIN Equipes on Equipes.Id == Partidas.TimeVisitantes_Id
+               INNER JOIN Equipes on Equipes.Id = Partidas.TimeCasa_Id
              WHERE TimeCasa_Id = ?
         ''',
-        [time_id]
+        [time_id],
+        one=True
+
     )
 
 
@@ -54,3 +43,68 @@ def ver_resultado(partida_id):
 #                 INNER JOIN Equipes on Equipes.Id == Partidas.TimeVisitantes_Id
 #              WHERE Id = ?
 #         ''',
+
+
+def listar_times():
+    return g.query_db(
+        'SELECT * FROM Equipes ORDER BY Pontos DESC'
+    )
+
+
+def obter_time(time_id):
+    return g.query_db(
+        '''SELECT * FROM Equipes WHERE Id = ?''',
+        [time_id],
+        one=True
+    )
+
+
+def novo_time(nome, sigla, localidade, pontos, jogos, vitorias, derrotas):
+    execute_db(
+        ''' INSERT INTO Equipes (Nome, Sigla, Localidade, Pontos, Jogos, Vitorias, Derrotas)
+            VALUES (?, ?, ?, ?, ?, ? , ?) ''',
+        (nome, sigla, localidade, pontos, jogos, vitorias, derrotas)
+    )
+
+
+def remover_time(time_id):
+    execute_db(
+        ''' DELETE FROM Equipes WHERE Id = ? ''',
+        (time_id)
+    )
+
+
+def update_time(nome, sigla, localidade, pontos, jogos, vitorias, derrotas, time_id):
+    execute_db(
+            '''
+            UPDATE Equipes
+            SET Nome = ?,
+                 Sigla = ?,
+                 Localidade = ?,
+                 Pontos = ?,
+                 Jogos = ?,
+                 Vitorias = ?,
+                 Derrotas = ?
+            WHERE Id = ?
+              ''',
+        (nome, sigla, localidade, pontos, jogos, vitorias, derrotas, time_id)
+    )
+
+
+def nova_partida(idTimeCasa, pontosCasa, idTimeVisita, pontosVisitantes, data, localpartida, duracao, setstotal, setsvencidos, setsperdidos, arbitro, fiscal, vencedor):
+    execute_db(
+        '''
+        INSERT INTO Partidas
+        (TimeCasa_Id, TimeVisitantes_Id, Pontos_Casa, Pontos_Visitante, DataJogo, LocalJogo, Duracao, SetsTotal, SetsVencidos, SetsPerdidos, ArbitroPrincipal, FiscalRede, Vencedor)
+        VALUES
+        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''',
+        (idTimeCasa, idTimeVisita, pontosCasa, pontosVisitantes, data, localpartida, duracao, setstotal, setsvencidos, setsperdidos, arbitro, fiscal, vencedor)
+    )
+
+
+def remover_partida(partida_id):
+    execute_db(
+        ''' DELETE FROM Partidas WHERE Id = ? ''',
+        (partida_id)
+    )
